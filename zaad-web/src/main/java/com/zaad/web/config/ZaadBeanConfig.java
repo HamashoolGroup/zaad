6a55705468701d2faf59d271c37d43263f444607
Junyi Song <socurites@island.local>
2016-04-06 02:17:13 +0900

    ImmutableSettings is deleted, and ues Settings; TransportClient was changed into builder pattern, and to use InetAddress

--- zaad-web/src/main/java/com/zaad/web/config/ZaadBeanConfig.java
+++ zaad-web/src/main/java/com/zaad/web/config/ZaadBeanConfig.java
@@ -1,5 +1,8 @@
 package com.zaad.web.config;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -23,16 +26,21 @@ public class ZaadBeanConfig {
 		ES_PORT = ZaadProperties.getAsInt("es.port");
 	}
 	
-	@SuppressWarnings("resource")
 	@Bean(destroyMethod = "close")
 	public TransportClient transportClient() {
-		Settings settings = ImmutableSettings.settingsBuilder()
+		Settings settings = Settings.settingsBuilder()
 				.put("cluster.name", ES_CLUSTER_NAME)
 				.build()
 		;
 		
-		return new TransportClient(settings)
-			.addTransportAddress(new InetSocketTransportAddress(ES_HOST, ES_PORT))
-		;
+		try {
+			return TransportClient.builder().settings(settings).build()
+				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ES_HOST), ES_PORT))
+			;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
