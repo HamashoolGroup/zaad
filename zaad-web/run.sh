872d609e85e14ac5cdce7f6cccb7cf4ccf1491d5
kwangsik <lks21c@gmail.com>
2016-04-07 14:31:41 +0900

    refactoring

--- zaad-web/run.sh
+++ zaad-web/run.sh
@@ -1,2 +1,2 @@
 git pull
-mvn -Des.cluaster.name=local -Des.host=localhost -Des.port=9300 -Dserver.port=8081 clean package spring-boot:run
+mvn -Dmaven.test.skip=true -Des.cluaster.name=local -Des.host=localhost -Des.port=9300 -Dserver.port=8081 clean package spring-boot:run
